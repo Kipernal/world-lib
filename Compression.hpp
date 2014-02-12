d5bba6153376370c76ceb13f45803e081a621761
Kipernal <kipernal@gmail.com>
2014-02-11 21:58:44 -0500

    Properly added compression function declarations to the header file

--- Compression.hpp
+++ Compression.hpp
@@ -2,6 +2,13 @@
 
 #include <vector>
 
+#ifndef WORLDLIB_IGNORE_DLL_FUNCTIONS
+#include <Windows.h>
+#include "../DLLcode/LunarDLL.h"
+#undef max
+#undef min
+#endif
+
 namespace worldlib
 {
 
@@ -28,7 +35,7 @@ namespace worldlib
 	///
 	/// \throws std::runtime_error An error occurred while decompressing the data.  Either there was an unrecognized bit sequence or there was not enough data to decompress
 	///
-	/// \see decompressGraphicsFile
+	/// \see decompressGraphicsFile, decompressData
 	///
 	////////////////////////////////////////////////////////////
 	template <typename inputIteratorType, typename outputIteratorType>
@@ -48,12 +55,113 @@ namespace worldlib
 	///
 	/// \throws std::runtime_error An error occurred while decompressing the data.  Either there was an unrecognized bit sequence or there was not enough data to decompress
 	///
-	/// \see decompressGraphicsFile
+	/// \see decompressGraphicsFile, decompressData
 	///
 	////////////////////////////////////////////////////////////
 	template <typename inputIteratorType, typename outputIteratorType>
 	outputIteratorType decompressLZ3(inputIteratorType compressedDataStart, inputIteratorType compressedDataEnd, outputIteratorType out, int *compressedSize = nullptr, int *decompressedSize = nullptr);
 
+	////////////////////////////////////////////////////////////
+	/// \brief Decompresses data compressed in the format the ROM uses 
+	///
+	/// \param romStart		An iterator pointing to the start of your ROM data
+	/// \param romEnd		An iterator pointing to the end of your ROM data
+	/// \param compressedDataStart	An iterator pointing to the beginning of the compressed data
+	/// \param compressedDataEnd	An iterator pointing to the end of the compressed data or any valid point after that (for example, the end of the ROM).
+	/// \param out			Where to output the data
+	/// \param compressedSize	Will contain the size of the compressed data after the function ends if it is not nullptr
+	/// \param decompressedSize	Will contain the size of the decompressed data after the function ends if it is not nullptr
+	///
+	/// \return Iterator pointing to the end of your decompressed data
+	///
+	/// \throws std::runtime_error An error occurred while decompressing the data.  Either there was an unrecognized bit sequence or there was not enough data to decompress
+	///
+	/// \see decompressGraphicsFile
+	///
+	////////////////////////////////////////////////////////////
+	template <typename romIteratorType, typename inputIteratorType, typename outputIteratorType>
+	outputIteratorType decompressData(romIteratorType romStart, romIteratorType romEnd, inputIteratorType compressedDataStart, inputIteratorType compressedDataEnd, outputIteratorType out, int *compressedSize = nullptr, int *decompressedSize = nullptr);
+
+	// Compression functions all rely on the Lunar Compress DLL
+	#ifndef WORLDLIB_IGNORE_DLL_FUNCTIONS
+
+	namespace internal
+	{
+		////////////////////////////////////////////////////////////
+		/// \brief Compresses data compressed in any format compatible with Lunar Compress
+		///
+		/// \param rawDataStart		An iterator pointing to the beginning of the raw data to compress
+		/// \param rawDataEnd		An iterator pointing to the end of the raw data to compress
+		/// \param out			Where to output the compressed data
+		/// \compressionType		The compression type. See LunarDLL.h
+		/// \param compressedSize	Will contain the size of the compressed data after the function ends if it is not nullptr
+		///
+		/// \return Iterator pointing to the end of your compressed data
+		///
+		/// \throws std::runtime_error Lunar Compress.dll could not be loaded or there was an error compressing the data.  The first is more likely.
+		///
+		////////////////////////////////////////////////////////////
+		template <typename inputIteratorType, typename outputIteratorType>
+		outputIteratorType compressGeneral(inputIteratorType rawDataStart, inputIteratorType rawDataEnd, outputIteratorType out, int compressionType, int *compressedSize);
+	}
+
+	////////////////////////////////////////////////////////////
+	/// \brief Compresses data compressed in the LZ2 format.
+	/// \details Please note that this function requires Lunar Compress.dll.  See also compressData, which takes into account the ROM's current compression type.
+	///
+	/// \param rawDataStart		An iterator pointing to the beginning of the raw data to compress
+	/// \param rawDataEnd		An iterator pointing to the end of the raw data to compress
+	/// \param out			Where to output the compressed data
+	/// \param compressedSize	Will contain the size of the compressed data after the function ends if it is not nullptr
+	///
+	/// \return Iterator pointing to the end of your compressed data
+	///
+	/// \throws std::runtime_error Lunar Compress.dll could not be loaded or there was an error compressing the data.  The first is more likely.
+	///
+	/// \see compressData
+	///
+	////////////////////////////////////////////////////////////
+	template <typename inputIteratorType, typename outputIteratorType>
+	outputIteratorType compressLZ2(inputIteratorType rawDataStart, inputIteratorType rawDataEnd, outputIteratorType out, int *compressedSize);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Compresses data compressed in the LZ3 format.
+	/// \details Please note that this function requires Lunar Compress.dll.  See also compressData, which takes into account the ROM's current compression type.
+	///
+	/// \param rawDataStart		An iterator pointing to the beginning of the raw data to compress
+	/// \param rawDataEnd		An iterator pointing to the end of the raw data to compress
+	/// \param out			Where to output the compressed data
+	/// \param compressedSize	Will contain the size of the compressed data after the function ends if it is not nullptr
+	///
+	/// \return Iterator pointing to the end of your compressed data
+	///
+	/// \throws std::runtime_error Lunar Compress.dll could not be loaded or there was an error compressing the data.  The first is more likely.
+	///
+	/// \see compressData
+	///
+	////////////////////////////////////////////////////////////
+	template <typename inputIteratorType, typename outputIteratorType>
+	outputIteratorType compressLZ3(inputIteratorType rawDataStart, inputIteratorType rawDataEnd, outputIteratorType out, int *compressedSize);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Compresses data compressed in the compression format your ROM uses format.
+	/// \details Please note that this function requires Lunar Compress.dll.  See also compressData, which takes into account the ROM's current compression type.
+	///
+	/// \param rawDataStart		An iterator pointing to the beginning of the raw data to compress
+	/// \param rawDataEnd		An iterator pointing to the end of the raw data to compress
+	/// \param out			Where to output the compressed data
+	/// \param compressedSize	Will contain the size of the compressed data after the function ends if it is not nullptr
+	///
+	/// \return Iterator pointing to the end of your compressed data
+	///
+	/// \throws std::runtime_error Lunar Compress.dll could not be loaded or there was an error compressing the data.  The first is more likely.
+	///
+	////////////////////////////////////////////////////////////
+	template <typename romIteratorType, typename inputIteratorType, typename outputIteratorType>
+	outputIteratorType compressData(romIteratorType romStart, romIteratorType romEnd, inputIteratorType rawDataStart, inputIteratorType rawDataEnd, outputIteratorType out, int *compressedSize);
+
+
+	#endif
 
 
 //////////////////////////////////////////////////////////////////////////////
@@ -61,4 +169,4 @@ namespace worldlib
 //////////////////////////////////////////////////////////////////////////////
 }
 
-#include "decompress.inl"
+#include "Compression.inl"
